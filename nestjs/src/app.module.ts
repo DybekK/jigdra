b9f9c4f9fb27dde965057c99bb1f7f0278629a52
DybeKk <matdybowski@gmail.com>
2021-07-03 18:44:22 +0200

    removed Sequelize and added Prisma

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,28 +1,15 @@
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
-import {ConfigModule} from "@nestjs/config";
-import configuration from "./config/configuration";
-
-const {database} = configuration();
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './domain/prisma.service';
 
 @Module({
   imports: [
-      ConfigModule.forRoot({
-        load: [configuration],
-        envFilePath: '.env',
-        isGlobal: true
-      }),
-      SequelizeModule.forRoot({
-        dialect: 'postgres',
-        host: database.host,
-        port: database.port,
-        username: database.username,
-        password: database.password,
-        database: database.name,
-        models: [],
-      })
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
   ],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
