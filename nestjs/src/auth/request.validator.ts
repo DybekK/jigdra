82224318761a754317a6d0734f1b827afeb24448
DybekK <matdybowski@gmail.com>
2021-07-04 00:27:36 +0200

    added test configuration and added unit tests for auth guard

--- nestjs/src/auth/request.validator.ts
+++ nestjs/src/auth/request.validator.ts
@@ -4,23 +4,16 @@ import {UnauthorizedException} from "@nestjs/common";
 import {UserContext} from "./auth.context";
 
 /**
- * @throws UnauthorizedException
- */
-const getUserContext = (accessToken: string): UserContext => {
-    try {
-        return <UserContext>(verify(accessToken, process.env.JWT_SECRET));
-    } catch (ex) {
-        throw new UnauthorizedException(ex.message);
-    }
-}
-
-/**
  * verifies if request has valid token, if so userContext is added to request
  * @throws UnauthorizedException
  */
 export const validateRequest = (request: Request): Request => {
     const bearerHeader = request.headers.authorization;
     const accessToken = bearerHeader && bearerHeader.split(' ')[1];
-    request.userContext = getUserContext(accessToken);
-    return request;
+    try {
+        request.userContext = <UserContext>(verify(accessToken, process.env.JWT_SECRET));
+        return request;
+    } catch (ex) {
+        throw new UnauthorizedException(ex.message);
+    }
 }
