9f1fb540a2c864453e9fc75874c7f060939a700c
DybekK <matdybowski@gmail.com>
2021-07-04 13:09:50 +0200

    added simple structure and created auth module for register and login

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  template: `
+    <app-login-section></app-login-section>
+  `,
 })
 export class AppComponent {
-  title = 'angular';
 }
