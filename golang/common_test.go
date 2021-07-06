5cb00e8ff9c9f0236bbc832519ffdbb2323e1f88
szymonorz <bvr0day@protonmail.com>
2021-07-07 01:47:29 +0200

    Removed unnecessary functions from common_test.go

--- golang/common_test.go
+++ golang/common_test.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"golang/model"
-	"time"
 
 	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func getRouter() *gin.Engine {
@@ -24,39 +18,4 @@ func getMiddleWare() (*jwt.GinJWTMiddleware, error) {
 		return nil, err
 	}
 	return middleware, nil
-}
-
-func getMongoClient() *mongo.Client {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://admin:password@localhost:27017"))
-	if err != nil {
-		panic(err)
-	}
-	return client
-}
-
-func createIndex(coll *mongo.Collection) {
-	//Username should also be unique
-	for _, key := range []string{"username", "email"} {
-		index, err := coll.Indexes().CreateOne(
-			context.Background(),
-			mongo.IndexModel{
-				Keys:    bson.D{{Key: key, Value: 1}},
-				Options: options.Index().SetUnique(true),
-			},
-		)
-
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println(index)
-	}
-}
-
-func GetUserCollection() *mongo.Collection {
-	client := getMongoClient()
-	coll := client.Database("jigdra_test").Collection("users")
-	createIndex(coll)
-	return coll
 }
