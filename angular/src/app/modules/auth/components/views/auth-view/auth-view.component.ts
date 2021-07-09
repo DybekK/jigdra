892d0fd967b98383f9989367f9adf12d0c803677
DybeKk <matdybowski@gmail.com>
2021-07-09 18:20:52 +0200

    small changes of css, made a better responsive look of auth page

--- angular/src/app/modules/auth/components/views/auth-view/auth-view.component.ts
+++ angular/src/app/modules/auth/components/views/auth-view/auth-view.component.ts
@@ -4,12 +4,12 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-auth-view',
   template: `
       <nz-layout nz-row class="auth-view">
-        <nz-layout nz-col [nzSm]="24" [nzMd]="14" class="auth-view__inner-content">
+        <nz-layout nz-col [nzSm]="24" [nzMd]="13" class="auth-view__inner-content">
           <app-auth-header></app-auth-header>
           <app-auth-content></app-auth-content>
           <app-footer></app-footer>
         </nz-layout>
-        <nz-layout nz-col [nzSm]="24" [nzMd]="10" class="auth-view__sider">
+        <nz-layout nz-col [nzSm]="24" [nzMd]="11" class="auth-view__sider">
           <h2 nz-typography>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </h2>
