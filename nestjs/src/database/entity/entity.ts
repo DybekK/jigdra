5e66cf1b51eca6f6bf1a2020d7f6ada7313a6bc2
DybekK <matdybowski@gmail.com>
2021-07-12 18:48:19 +0200

    added new relations to entities

--- nestjs/src/database/entity/entity.ts
+++ nestjs/src/database/entity/entity.ts
@@ -1,10 +1,13 @@
 import {Column, Entity, Index, ManyToOne, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+import {worker} from "cluster";
 
-@Entity()
-export class WorkspaceUser {
+export abstract class Model {
     @PrimaryGeneratedColumn("uuid")
     id: string;
+}
 
+@Entity()
+export class WorkspaceUser extends Model {
     @Index({unique: true})
     @Column()
     userId: string
@@ -17,16 +20,33 @@ export class WorkspaceUser {
 
     @ManyToOne(() => Workspace, workspace => workspace.workspaceUsers)
     workspace: Promise<Workspace>;
+
+    @OneToMany(() => Task, task => task.workspace)
+    tasks: Promise<Task[]>;
 }
 
 @Entity()
-export class Workspace {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
-
+export class Workspace extends Model {
     @Column()
     name: string
 
     @OneToMany(() => WorkspaceUser, workspaceUser => workspaceUser.workspace)
     workspaceUsers: Promise<WorkspaceUser[]>;
+
+    @OneToMany(() => Task, task => task.workspace)
+    tasks: Promise<Task[]>;
+}
+
+export class Task {
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @ManyToOne(() => Workspace, workspace => workspace.tasks)
+    workspace: Promise<Workspace>;
+
+    @ManyToOne(() => WorkspaceUser, workspaceUser => workspaceUser.tasks)
+    workspaceUser: Promise<WorkspaceUser>
 }
