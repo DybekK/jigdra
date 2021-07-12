432767a7bd7593d2813878cb5b404a664f9fb79a
Dawid Kiełkowski <dawid55a@gmail.com>
2021-07-12 23:10:13 +0200

    added first draft for task creation fields

--- angular/src/app/modules/user/components/organisms/user-content/user-content.component.ts
+++ angular/src/app/modules/user/components/organisms/user-content/user-content.component.ts
@@ -1,18 +1,100 @@
-import {Component, OnInit} from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {NzMarks, NzSliderValue} from "ng-zorro-antd/slider";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-user-content',
   template: `
-    <h2>content works</h2>
+    <form [formGroup]="createTask" class="ant-advanced-search-form">
+      <div nz-row nzJustify="center" [nzGutter]="[vGutter, hGutter]">
+        <div nz-col [nzSpan]="20">
+          <nz-form-item>
+            <nz-form-control nzErrorTip="You need to specify task name">
+              <nz-input-group nzAddOnBefore="Task">
+                <input type="text" nz-input formControlName="taskName" placeholder="Task name"/>
+              </nz-input-group>
+            </nz-form-control>
+          </nz-form-item>
+        </div>
+      </div>
+      <div nz-row [nzGutter]="[vGutter, hGutter]">
+        <div nz-col [nzSpan]="24">
+          <nz-form-item>
+            <nz-form-control>
+              <nz-slider formControlName="time" [nzMarks]="timeStamps" [nzStep]="1" nzMax="120"
+                         (nzOnAfterChange)="getTimeValue($event)"></nz-slider>
+            </nz-form-control>
+          </nz-form-item>
+        </div>
+      </div>
+      <div nz-row nzJustify="center" [nzGutter]="[vGutter, hGutter]">
+        <div nz-col [nzSpan]="10">
+          <nz-form-item>
+            <nz-form-control nzErrorTip="Number of brakes">
+              <nz-input-group nzAddOnBefore="Breaks">
+                <input type="number" nz-input formControlName="taskName" placeholder="Task name"/>
+              </nz-input-group>
+            </nz-form-control>
+          </nz-form-item>
+        </div>
+        <div nz-col [nzSpan]="10">
+          <nz-form-item>
+            <nz-form-control nzErrorTip="You need to specify task name">
+              <nz-input-group nzAddOnBefore="Lorem">
+                <input type="text" nz-input placeholder="Lorem ipsum"/>
+              </nz-input-group>
+            </nz-form-control>
+          </nz-form-item>
+        </div>
+      </div>
+      <div nz-row nzJustify="center" [nzGutter]="[vGutter, hGutter]">
+        <div nz-col [nzSpan]="20">
+          <nz-form-item>
+              <textarea nz-input placeholder="Write your comment here!"
+                        [nzAutosize]="{ minRows: 2, maxRows: 5 }"></textarea>
+          </nz-form-item>
+        </div>
+      </div>
+      <div nz-row nzJustify="center" [nzGutter]="[vGutter, hGutter]">
+        <div nz-col [nzSpan]="20">
+            <button nz-button [nzType]="'primary'">Crate task</button>
+        </div>
+      </div>
+    </form>
   `,
   styleUrls: ['./user-content.component.scss']
 })
 export class UserContentComponent implements OnInit {
 
-  constructor() {
+  createTask!: FormGroup;
+  hGutter = {xs: 8, sm: 16, md: 24};
+  vGutter = {xs: 8, sm: 16, md: 24};
+
+  timeStamps: NzMarks = {
+    0: '0 min',
+    10: '10 min',
+    25: '25 min',
+    50: '50 min'
+  }
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  /*
+   * Get time value from slider as **number**
+   */
+  getTimeValue(value: NzSliderValue) {
+    console.log(value);
   }
 
   ngOnInit(): void {
+    this.createTask = this.fb.group({
+      taskName: new FormControl(),
+      time: new FormControl()
+    });
   }
 
 }
