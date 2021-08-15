f83d0bc4ef7998d42c4c2c325654e062065c04b5
szymonorz <bvr0day@protonmail.com>
2021-08-15 23:07:22 +0200

    Fixed errors

--- golang/model/repository/user-repository.go
+++ golang/model/repository/user-repository.go
@@ -2,21 +2,15 @@ package repository
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"golang/model/dto"
-	"regexp"
-	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type userServ struct{}
-
 type UserRepository interface {
-	ValidateEmail(string) error
 	CreateUser(*dto.User, context.Context) (string, error)
 	GetUser(*dto.LoginUser, context.Context) (*dto.User, error)
 	GetUserById(string, context.Context) (*dto.GetUserStruct, error)
@@ -29,61 +23,22 @@ func NewUserRepository() UserRepository {
 	return &database{}
 }
 
-func (d *database) ValidateEmail(email string) error {
-	match := dto.Email_regex.MatchString(email)
-	if match {
-		return nil
-	} else {
-		return errors.New("invalid email")
-	}
-}
-
 func (d *database) GetUser(login *dto.LoginUser, ctx context.Context) (*dto.User, error) {
 	res := UserCollection.FindOne(ctx, bson.M{
 		"email": login.Email,
 	})
-
 	var result dto.User
 	decode_err := res.Decode(&result)
 	if decode_err != nil {
 		return nil, decode_err
 	}
+	return &result, nil
 
 }
 
 func (d *database) CreateUser(req_user *dto.User, ctx context.Context) (string, error) {
-	req_user.Id = primitive.NewObjectID()
-	email_err := d.ValidateEmail(req_user.Email)
-	if email_err != nil {
-		return "", email_err
-	}
-
-	var hash, hash_error = hashPassword(req_user.Password)
-	if hash_error != nil {
-		return "", errors.New("hashing error")
-	}
-
-	if availible := isUsernameAvailable(req_user.Username, ctx); !availible {
-		return "", errors.New("username taken")
-	}
-	req_user.Password = hash
 	result, insertError := UserCollection.InsertOne(ctx, &req_user)
-	if insertError != nil {
-		matched, _ := regexp.MatchString(`duplicate key`, insertError.Error())
-		if matched {
-			return "", errors.New("409")
-
-		} else {
-			return "", errors.New("failed to insert user")
-		}
-	}
-	id := strings.Split(fmt.Sprintf("%v", result), "\"")[1]
-	// hex, err := RedirectService.SecureRedirect(ctx, id)
-	// if err != nil {
-	// 	return "", errors.New("unable to redirect")
-	// }
-
-	return id, nil
+	return fmt.Sprintf("%v", result), insertError
 }
 
 func (d *database) GetUserById(id string, ctx context.Context) (*dto.GetUserStruct, error) {
--- golang/model/user.go
+++ golang/model/user.go
@@ -6,12 +6,16 @@ import (
 	"fmt"
 	"golang/model/dto"
 	"golang/model/repository"
+	"regexp"
+	"strings"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService interface {
-	ValidateEmail(string) error
 	CreateUser(*dto.User, context.Context) (string, error)
 	GetUser(*dto.LoginUser, context.Context) (*dto.User, error)
 	GetUserById(string, context.Context) (*dto.GetUserStruct, error)
@@ -24,12 +28,6 @@ type userService struct {
 func NewUserService(repo repository.UserRepository) UserService {
 	return &userService{repo: repo}
 }
-
-func (u *userService) ValidateEmail(email string) error {
-	return u.repo.ValidateEmail(email)
-
-}
-
 func (u *userService) GetUser(login *dto.LoginUser, ctx context.Context) (*dto.User, error) {
 	user, err := u.repo.GetUser(login, ctx)
 	if err != nil {
@@ -43,7 +41,34 @@ func (u *userService) GetUser(login *dto.LoginUser, ctx context.Context) (*dto.U
 }
 
 func (u *userService) CreateUser(req_user *dto.User, ctx context.Context) (string, error) {
-	return u.repo.CreateUser(req_user, ctx)
+	req_user.Id = primitive.NewObjectID()
+	email_err := validateEmail(req_user.Email)
+	if email_err != nil {
+		return "", email_err
+	}
+
+	var hash, hash_error = hashPassword(req_user.Password)
+	if hash_error != nil {
+		return "", errors.New("hashing error")
+	}
+
+	if availible := isUsernameAvailable(req_user.Username, ctx); !availible {
+		return "", errors.New("username taken")
+	}
+	req_user.Password = hash
+	res, err := u.repo.CreateUser(req_user, ctx)
+	if err != nil {
+		matched, _ := regexp.MatchString(`duplicate key`, err.Error())
+		if matched {
+			return "", errors.New("409")
+
+		} else {
+			return "", errors.New("failed to insert user")
+		}
+	}
+
+	id := strings.Split(res, "\"")[1]
+	return id, nil
 }
 
 func (u *userService) GetUserById(id string, ctx context.Context) (*dto.GetUserStruct, error) {
@@ -59,6 +84,11 @@ func verifyPassword(password, hash string) bool {
 	fmt.Println(hash)
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func isUsernameAvailable(username string, ctx context.Context) bool {
+	result := repository.UserCollection.FindOne(ctx, bson.M{"username": username})
+	return result.Err() == mongo.ErrNoDocuments
 }
 
 func validateEmail(email string) error {
